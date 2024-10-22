7373deea151341d2f579a19b87603b8476a1c2df
kcho7511 <kcho7511@gmail.com>
2024-10-22 19:07:24 +0900

    add member\Main.java

--- src/module-info.java
+++ src/module-info.java
@@ -6,6 +6,10 @@
  */
 module FOUR_GUYS {
 	// 외부 패키지 사용 : JDK 1.9
+	requires JMember;
+	requires ojdbc8;
+	requires com.google.gson;
+	requires org.jsoup;
 	requires org.apache.logging.log4j;
 	requires org.apache.logging.log4j.core;
 }
